52d6ca0c34074ce51c5bca5688a84f480412a939
Ui <uipt1116@gmail.com>
2026-03-28 11:53:29 +0900

    add

---
+++ REIGI/REIGIApp.swift
@@ -0,0 +1,17 @@
+//
+//  REIGIApp.swift
+//  REIGI
+//
+//  Created by 渡邉羽唯 on 2026/03/28.
+//
+
+import SwiftUI
+
+@main
+struct REIGIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
